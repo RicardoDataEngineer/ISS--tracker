03fa0542ce144595495f877c705005282debc155
Ricardo Moreira <hicardomoreira@gmail.com>
2024-02-24 15:09:02 -0300

    Step 1

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -1,3 +1,19 @@
 import requests
+import datetime
 
 reponse = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+# Connection error status
+reponse.raise_for_status()
+
+data = reponse.json()
+print(data)
+
+timestamp = data["timestamp"]
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+date_time = datetime.datetime.utcfromtimestamp(timestamp)
+iss_position = (longitude, latitude)
+
+print(iss_position)
+print(date_time)
