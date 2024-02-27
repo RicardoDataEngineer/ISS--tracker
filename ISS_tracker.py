dd881c39ebf29cff5da119e06540dbd536fd8bd0
Ricardo Moreira <hicardomoreira@gmail.com>
2024-02-27 10:09:42 -0300

    Step2

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -1,13 +1,14 @@
+#%%
 import requests
+import folium
 import datetime
-
 reponse = requests.get(url="http://api.open-notify.org/iss-now.json")
 
 # Connection error status
 reponse.raise_for_status()
 
 data = reponse.json()
-print(data)
+#print(data)
 
 timestamp = data["timestamp"]
 longitude = data["iss_position"]["longitude"]
@@ -16,4 +17,17 @@ date_time = datetime.datetime.utcfromtimestamp(timestamp)
 iss_position = (longitude, latitude)
 
 print(iss_position)
-print(date_time)
+#print(date_time)
+
+
+mapObj = folium.Map(location=[latitude,longitude], zoom_start=2)
+folium.Marker(location=[latitude,longitude]).add_to(mapObj)
+mapObj.save("ISS.html")
+
+
+
+
+
+
+
+# %%
